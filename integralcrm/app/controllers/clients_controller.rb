f1c6981bf40583b2229e1e5961ab8c2a5f9649b1
Edward Heaver <edward.heaver01@gmail.com>
2020-01-06 19:31:19 +0000

    Completed Clients controller

--- integralcrm/app/controllers/clients_controller.rb
+++ integralcrm/app/controllers/clients_controller.rb
@@ -1,37 +1,64 @@
 class ClientsController < ApplicationController
 
   # GET: /clients
-  get "/clients" do
-    erb :"/clients/index.html"
+  get "/clients/index" do
+    @clients = Client.all
+    erb :"/clients/index", :layout => :"/layouts/landing"
   end
 
   # GET: /clients/new
   get "/clients/new" do
-    erb :"/clients/new.html"
+    erb :"/clients/new", :layout => :"/layouts/landing"
   end
 
   # POST: /clients
   post "/clients" do
-    redirect "/clients"
+    
+    @client = Client.create(
+      :name => params[:name],
+      :contact_number => params[:contact_number],
+      :address1 => params[:address1],
+      :address2 => params[:address2],
+      :city => params[:city],
+      :postcode => params[:postcode],
+      :email => params[:email],
+      :add_info => params[:add_info])
+    @client.save
+    redirect "/dashboard"
   end
 
   # GET: /clients/5
   get "/clients/:id" do
-    erb :"/clients/show.html"
+    @client = Client.find_by_id(params[:id])
+    erb :"/clients/show", :layout => :"/layouts/landing"
   end
 
   # GET: /clients/5/edit
   get "/clients/:id/edit" do
-    erb :"/clients/edit.html"
+    @client = Client.find_by_id(params[:id])
+    erb :"/clients/edit", :layout => :"layouts/landing"
   end
 
   # PATCH: /clients/5
-  patch "/clients/:id" do
-    redirect "/clients/:id"
+  post "/clients/:id" do
+    binding.pry
+    @client = Client.find_by_id(params[:id])
+    @client.name = params[:name]
+    @client.contact_number = params[:contact_number]
+    @client.email = params[:email]
+    @client.address1 = params[:address1]
+    @client.address2 = params[:address2]
+    @client.postcode = params[:postcode]
+    @client.city = params[:city]
+    @client.add_info = params[:add_info]
+    @client.save
+    redirect "/clients/index"
   end
 
   # DELETE: /clients/5/delete
-  delete "/clients/:id/delete" do
-    redirect "/clients"
+  post "/clients/:id/delete" do
+    @client = Client.find_by_id(params[:id])
+    @client.destroy
+    redirect "/clients/index"
   end
 end
