0d0f8baf55a0c14b8b6ecdb33bf88e17cbbb6720
Edward Heaver <edward.heaver01@gmail.com>
2020-01-10 15:16:50 +0000

    completed notes controller

--- integralcrm/app/controllers/notes_controller.rb
+++ integralcrm/app/controllers/notes_controller.rb
@@ -2,36 +2,70 @@ class NotesController < ApplicationController
 
   # GET: /notes
   get "/notes" do
-    erb :"/notes/index.html"
+    @notes = Note.all
+    erb :"/notes/index", :layout => :"/layouts/landing"
   end
 
   # GET: /notes/new
   get "/notes/new" do
-    erb :"/notes/new.html"
+    erb :"/notes/new", :layout => :"/layouts/landing"
   end
+
+  get "/clients/notes/new" do
+    erb :"/notes/new", :layout => :"/layouts/landing"
+  end
+
+  get "/cases/notes/new" do
+    erb :"/notes/new", :layout => :"/layouts/landing"
+  end
+
+  post "/clients/notes" do
+    
 
   # POST: /notes
   post "/notes" do
+    @note = Note.create(params[:note])
+    @note.save
     redirect "/notes"
   end
 
   # GET: /notes/5
   get "/notes/:id" do
-    erb :"/notes/show.html"
+    @note = Note.find_by_id(params[:id])
+    erb :"/notes/show", :layout => :"/layouts/landing"
   end
 
   # GET: /notes/5/edit
   get "/notes/:id/edit" do
-    erb :"/notes/edit.html"
+    @note = Note.find_by_id(params[:id])
+    erb :"/notes/edit", :layout => :"/layouts/landing"
   end
 
   # PATCH: /notes/5
-  patch "/notes/:id" do
-    redirect "/notes/:id"
+  post "/notes/:id" do
+      @note = Note.find_by_id(params[:id])
+      if session[:user_id] == @note.owner
+        @note.update(params[:note])
+        @note.save
+      else
+        "Cannot update another users resource"
+        redirect "/notes/:id"
+      end
+    
   end
 
   # DELETE: /notes/5/delete
-  delete "/notes/:id/delete" do
-    redirect "/notes"
+  post "/notes/:id/delete" do
+    @note = Note.find_by_id(params[:id])
+    if session[:user_id] == @note.owner
+      @note.destroy
+      redirect "/landing"
+    else
+      "can't delete other users notes"
+    end
+
   end
+
+
+
 end
